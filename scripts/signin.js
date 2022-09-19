cc952790608c78aaca966ab1d937db62ebabfa17
GabieRise <merrygabby14@gmail.com>
2022-09-19 14:11:55 +0100

    initial commit

---
+++ scripts/signin.js
@@ -0,0 +1,9 @@
+const firstRow = document.querySelector(".first-row")
+const secondRow = document.querySelector(".second-row")
+const thirdRow = document.querySelector(".third-row")
+const btn = document.querySelector(".btn")
+
+function Signin(){
+    if(firstRow.value !=="" && secondRow.value !=="" & thirdRow.value !=="") 
+    alert("welcome")
+}
---
+++ scripts/signup.js
@@ -0,0 +1,13 @@
+const login = document.querySelector(".login")
+const Name = document.querySelector("#name")
+const email = document.querySelector("#email")
+const pass = document.querySelector("#password")
+
+function signup(){
+    if(Name.value !=="" && email.value !=="" && password.value !=="")
+    login.style.display
+    else{
+        alert("fill the required field")
+    }
+
+}
